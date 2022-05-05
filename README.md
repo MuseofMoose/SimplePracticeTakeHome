9e9d56a2595c1415b0220115c2d5dddb28f52e9c
MuseofMoose <sdellis@u.rochester.edu>
2022-05-04 17:22:11 -0700

    Adding progress-side-bar test and updating ReadMe.

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # sp-take-home
 
 Take home assignment for SimplePractice interview.
+## Notes
+I used the endpoints provided in the assignment document to get an idea of the shape of the API and then did my best to replicate it in mirage for the sake of making development and testing easier (not having to deal with CORS is a plus, too). You can explore my mirage models/factories for further detail. There were some small assumptions I needed to make regarding the relationships between the back-end data given my inability to see the back-end code but nothing too daunting.
 
-## Some notable omissions from this assignment (due to time constraints):
+<br/>
+
+## Notable omissions from this assignment (due to time constraints):
 ### Testing
 - An acceptance test to cover the entire app flow.
-- Integration tests for the progress-side-bar component.
 
 ### App
 - Making it look less boring (particularly the side-bar).
-- Handling the browser back button. Clicking on the side bar to go back works but hitting back in your browser will take you to the previous route without resetting the state.
+- Handling the browser back button. Clicking on a completed step in the side bar to go back works but hitting back in your browser will take you to the previous route without resetting the state.
 
-## Prerequisites
+<br/>
+
+## Ember ReadMe Boilerplate
+#### Prerequisites
 
 You will need the following things properly installed on your computer.
 
@@ -20,29 +26,29 @@ You will need the following things properly installed on your computer.
 * [Ember CLI](https://cli.emberjs.com/release/)
 * [Google Chrome](https://google.com/chrome/)
 
-## Installation
+#### Installation
 
 * `git clone <repository-url>` this repository
 * `cd sp-take-home`
 * `npm install`
 
-## Running / Development
+#### Running / Development
 
 * `ember serve`
 * Visit your app at [http://localhost:4200](http://localhost:4200).
 * Visit your tests at [http://localhost:4200/tests](http://localhost:4200/tests).
 
-### Running Tests
+##### Running Tests
 
 * `ember test`
 * `ember test --server`
 
-### Linting
+##### Linting
 
 * `npm run lint`
 * `npm run lint:fix`
 
-### Building
+##### Building
 
 * `ember build` (development)
 * `ember build --environment production` (production)
--- tests/integration/components/progress-side-bar-test.js
+++ tests/integration/components/progress-side-bar-test.js
@@ -1,28 +1,114 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { ProgressStep } from 'sp-take-home/services/selection-progress';
+import sinon from 'sinon';
 
 module('Integration | Component | progress-side-bar', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  // This needs tests but I've unfortunately run up against my time limit.
+  hooks.beforeEach(async function () {
+    this.router = this.owner.lookup('service:router');
+    this.selectionProgress = this.owner.lookup('service:selection-progress');
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    const testSteps = [
+      new ProgressStep('step1', '/step1', true),
+      new ProgressStep('step2', '/step2'),
+    ];
+
+    this.selectionProgress.steps = testSteps;
 
     await render(hbs`<ProgressSideBar />`);
+  });
 
-    assert.dom(this.element).hasText('');
+  test('displays expected attributes', async function (assert) {
+    const displayValues = ['potato', 'horse'];
+    // Advancing a step so we have steps in a variety of different states.
+    this.selectionProgress.completeStep(displayValues);
+    await settled();
 
-    // Template block usage:
-    await render(hbs`
-      <ProgressSideBar>
-        template block text
-      </ProgressSideBar>
-    `);
+    assert
+      .dom('[data-test-step]')
+      .exists({ count: 2 }, 'should be two steps displayed');
 
-    assert.dom(this.element).hasText('template block text');
+    assert
+      .dom('[data-test-step="0"]')
+      .hasClass(/step/, 'completed step should have "step" class');
+    assert
+      .dom('[data-test-step="0"]')
+      .hasClass(/complete/, 'completed step should have "complete" class');
+    assert
+      .dom('[data-test-step="0"]')
+      .doesNotHaveClass(
+        /active/,
+        'completed step should not have "active" class'
+      );
+    assert
+      .dom('[data-test-step="0"] [data-test-checkmark-bullet]')
+      .isVisible('completed step should display checkmark bullet');
+    assert
+      .dom('[data-test-step="0"] [data-test-number-bullet]')
+      .doesNotExist('completed step should not display number bullet');
+    assert
+      .dom('[data-test-step="0"] [data-test-title]')
+      .hasText(
+        this.selectionProgress.steps[0].title,
+        'step 0 should have expected title'
+      );
+
+    displayValues.forEach((value, index) => {
+      assert
+        .dom(`[data-test-step="0"] [data-test-display-value="${index}"]`)
+        .hasText(value, `completed step should have display value: ${value}`);
+    });
+
+    assert
+      .dom('[data-test-step="1"]')
+      .hasClass(/step/, 'active step should have step class');
+    assert
+      .dom('[data-test-step="1"]')
+      .hasClass(/active/, 'active step should have active class');
+    assert
+      .dom('[data-test-step="1"]')
+      .doesNotHaveClass(
+        /complete/,
+        'active step should not have complete class'
+      );
+    assert
+      .dom('[data-test-step="1"] [data-test-checkmark-bullet]')
+      .doesNotExist('active step should not display checkmark bullet');
+    assert
+      .dom('[data-test-step="1"] [data-test-number-bullet]')
+      .isVisible('active step should display number bullet');
+    assert
+      .dom('[data-test-step="1"] [data-test-title]')
+      .hasText(
+        this.selectionProgress.steps[1].title,
+        'step 1 should have expected title'
+      );
+    assert
+      .dom('[data-test-step="1"] [data-test-display-value]')
+      .doesNotExist('active step should not have any display values');
+  });
+
+  test('calls goToStep() with expected index when clicking a completed step', async function (assert) {
+    sinon.stub(this.selectionProgress, 'goToStep');
+    this.selectionProgress.completeStep([]);
+    await settled();
+
+    const index = 0;
+    await click(`[data-test-step="${index}"]`);
+    const goToStepArgs = this.selectionProgress.goToStep.args[0];
+    assert.strictEqual(goToStepArgs[0], index);
+  });
+
+  test('does not call goToStep() when clicking an incomplete step', async function (assert) {
+    sinon.stub(this.selectionProgress, 'goToStep');
+    const index = 0;
+    await click(`[data-test-step="${index}"]`);
+    assert.strictEqual(this.selectionProgress.goToStep.callCount, 0);
   });
 });
